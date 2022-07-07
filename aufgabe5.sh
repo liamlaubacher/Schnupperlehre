797c31c422d7ad0ae77a0f9d7464143504fc55e3
liamlaubacher <laubacherliam@gmail.com>
2022-07-07 15:38:16 +0200

    improve  aufgabe5.sh

--- aufgabe5.sh
+++ aufgabe5.sh
@@ -1,14 +1,16 @@
 
 randomNumber=$(( ( RANDOM % 100 )  + 1 ))
 myNumber=200
-
-
+versuche=0
 while [ "$myNumber" -ne "$randomNumber" ]; do
-read -p "guess the number?" myNumber
-if [ "$randomNumber" -gt "$myNumber" ]; then
-    echo "higher"
-elif [ "$randomNumber" -lt "$myNumber" ]; then
-    echo "lower"   
+versuche=$((versuche+1))
+    read -p "guess the number?" myNumber
 
-fi 
+    if [ "$randomNumber" -gt "$myNumber" ]; then
+        echo "higher"
+    elif [ "$randomNumber" -lt "$myNumber" ]; then
+        echo "lower"   
+    fi
+
 done
+echo "congrats you guessed it right" "You needed $versuche times to guess it."
